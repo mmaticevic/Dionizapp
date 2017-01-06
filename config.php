8440b61be152ce54f476190f77ee61388a89d34f
Marko Maticevic <mmaticevic@gmail.com>
2017-01-06 21:20:38 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,30 @@
+ <?php
+include_once 'config.php';
+session_start();
+
+if($_SERVER["SERVER_NAME"]==="localhost"){
+	$putanja="/dionizapp/";
+	$server="localhost";
+	$imeBaze="b6_19192081_vina";
+	$korisnik="root";
+	$lozinka="statebriga11";
+}else{
+	$putanja="/Dionizapp/";
+	$server="sql211.byethost6.com";
+	$imeBaze="b6_19192081_vina";
+	$korisnik="b6_19192081";
+	$lozinka="statebriga11";
+}
+
+
+$veza = new PDO(
+	"mysql:host=" . $server . ";dbname=" . $imeBaze,
+	$korisnik,
+	$lozinka,
+	array(
+		PDO::ATTR_EMULATE_PREPARES=> false,
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+		PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET utf8",
+		PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+	)
+);
