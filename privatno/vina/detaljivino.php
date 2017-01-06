074cfc6c38cd9d99a3a36e9bbf63f4cbb45dc7ff
Marko Maticevic <mmaticevic@gmail.com>
2017-01-06 22:24:47 +0100

    Add files via upload

---
+++ privatno/vina/detaljivino.php
@@ -0,0 +1,22 @@
+					     
+<div class="col-sm-4">
+
+	<div class="product-image-wrapper">
+		<div class="single-products">
+			<div class="productinfo text-center">
+				<a href="#">  <img src="img/nepoznato.jpg" alt="" />
+				</a>
+				<h2><?php echo $vino->cijena ?> kn</h2>
+				<h5><?php echo $vino->sorta_vina ?></h5>
+				<p><?php echo $vino->naziv ?></p>
+
+				<h5>   <?php echo $vino->godina_berbe ?></h5>
+				<a href="#" class="btn btn-danger btn-block "><i class="fa fa-eraser" aria-hidden="true"></i></a>
+				<a href="#" class="btn btn-info btn-block "><i class="fa fa-refresh" aria-hidden="true"></i></a>
+				<a href="#" class="btn btn-success btn-block "><i class="fa fa-plus" aria-hidden="true"></i></a>
+			</div>
+			
+		</div>
+		
+	</div>
+</div>
